051749b2658c85612885a98ac45551d25edb5b90
Mace Akkari <maceplus@gmail.com>
2025-06-11 13:14:19 +0100

    feat: add header and hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
-  return <div>Top Roofing & Construction</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div id="top" className="sr-only">
+        Top of page
+      </div>
+      <Header />
+      <main className="flex-1">
+        <HeroSection />
+      </main>
+    </div>
+  );
 }
